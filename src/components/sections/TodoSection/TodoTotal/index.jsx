c27d540fc6699781d4d53fa9a1e15dacac6415c8
unknown <maxyurisantos42@gmail.com>
2023-10-31 14:16:16 -0300

    teste

--- src/components/sections/TodoSection/TodoTotal/index.jsx
+++ src/components/sections/TodoSection/TodoTotal/index.jsx
@@ -5,7 +5,7 @@ export const TodoTotal = ({ valueTotal }) => {
     return (
         <div className={styles.totalBox}>
             <div>
-                <p className="paragraph bold">Valor Total:</p>
+                <p className="paragraph bold">Valor Total Teste:</p>
                 <p className="paragraph small">O valor se refere ao saldo</p>
             </div>
             <span className="paragraph bold pinkColor">{valueTotal}</span>
